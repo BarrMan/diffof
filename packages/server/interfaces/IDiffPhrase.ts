27f9f8388ac8bb4ed8df5f9c8d52dae0ab38a00f
Imri Barr <imri.barr@naturalint.com>
2021-04-21 09:41:56 +0300

    re-built common

--- packages/server/interfaces/IDiffPhrase.ts
+++ packages/server/interfaces/IDiffPhrase.ts
@@ -1,5 +1,5 @@
 import DiffKind from "./DiffKind";
-import { PhraseSymbol } from "@barrman/diffof-common/dist";
+import { PhraseSymbol } from "@barrman/diffof-common";
 
 export default interface IDiffPhrase {
     diffKind?: DiffKind;
